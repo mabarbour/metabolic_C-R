45c0f12e557de98b74080edb22835a5e5b22b523
mabarbour <matthew.a.barbour@gmail.com>
2018-03-29 18:23:29 +0000

    create project and first equation. Still don't understand why Arrhenius equation gives a value of zero...

---
+++ equations.R
@@ -0,0 +1,13 @@
+
+C_to_K <- function(C) C + 273.15 # convert degrees C to Kelvin for Arrhenius equation
+
+arrhenius_eqn <- function(E_a,   # activation energy of metabolic processes, units = eV/K
+                          T)     # temperature in Kelvin
+{
+  euler <- exp(1)  # Euler's number
+  k <- 8.6173303 * 10^(-5) # Boltzmann constant, units = eV (electron volt)
+  
+  euler^(-E_a/k*T)
+}
+
+arrhenius_eqn(E_a = 0.65, T = C_to_K(20))
