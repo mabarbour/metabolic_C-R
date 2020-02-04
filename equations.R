51670c0b2eb0dab32169841716c84f84c72d99b2
mabarbour <matthew.a.barbour@gmail.com>
2020-02-04 15:54:38 +0100

    add example of scaling intrinsic growth rate

--- equations.R
+++ equations.R
@@ -11,3 +11,21 @@ arrhenius_eqn <- function(E_a,   # activation energy of metabolic processes, uni
 }
 
 arrhenius_eqn(E_a = 0.65, T = C_to_K(20))
+
+r0 <- function(r_base, # baseline r for a given temperature 
+               E_a, 
+               T){
+  # r_base = r0 * arrhenius_eqn to get observed r value, so:
+  r_base/arrhenius_eqn(E_a = E_a, T = T) # = r0
+}
+
+r_scaling <- function(r0, E_a, T){
+  r0*arrhenius_eqn(E_a = E_a, T = T)
+}
+
+# example of r scaling
+Temp_seq = 15:30
+r_seq <- r_scaling(r0 = r0(r_base = 2, E_a = 0.65, T = C_to_K(20)), # initialize r at 20 Celsius
+                   E_a = 0.65,
+                   T = C_to_K(Temp_seq))
+plot(r_seq ~ Temp_seq, type = "l")
